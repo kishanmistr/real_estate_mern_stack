ee47692d4064a84a65793b24cf9358a30c545fd4
kiritDholakiya <dolakiyakirit@gmail.com>
2024-07-17 15:56:09 +0530

    create pages and routes

---
+++ frontend/src/pages/Signin.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Signin = () => {
+
+  return (
+
+    <div>
+      
+    </div>
+    
+  )
+}
+
+export default Signin
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Signup = () => {
+
+  return (
+
+    <div>
+      
+    </div>
+    
+  )
+}
+
+export default Signup
